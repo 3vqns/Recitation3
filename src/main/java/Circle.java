e75b1012d04e63d7e37acab14fa8a5502842df01
Evans <earmantrading23@students.desu.edu>
2024-02-16 11:44:31 -0500

    Completed!

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -2,8 +2,8 @@
  * This class provides a simple Circle type that
  * creates and tracks instances of circles.
  *
- * @author (You & Me)
- * @version (2019-10-15)
+ * @author Evans
+ * @version (2024-2-16)
  */
 public class Circle
 {
@@ -17,7 +17,8 @@ public class Circle
      */
     public Circle()
     {
-
+        location = new Point(0,0);
+        radius = 1.0;
     }
 
     /**
@@ -28,7 +29,8 @@ public class Circle
      */
     public Circle(double radius)
     {
-
+        location = new Point(0,0);
+        setRadius(radius);
     }
 
     /**
@@ -40,7 +42,8 @@ public class Circle
      */
     public Circle(Point location, double radius)
     {
-
+        setLocation(location);
+        setRadius(radius);
     }
 
     //accessors
@@ -51,7 +54,7 @@ public class Circle
      */
     public double getRadius()
     {
-
+        return radius;
     }
 
     /**
@@ -61,7 +64,7 @@ public class Circle
      */
     public Point getLocation()
     {
-
+        return new Point(location);
     }
 
     // mutator methods
@@ -72,7 +75,7 @@ public class Circle
      */
     public void setRadius(double radius)
     {
-
+        this.radius = radius;
     }
 
     /**
@@ -82,7 +85,7 @@ public class Circle
      */
     public void setLocation(Point location)
     {
-
+        this.location = new Point(location);
     }
 
     // other methods
@@ -91,10 +94,14 @@ public class Circle
      * falling on the circle is considered inside the circle.
      *
      * @return true if the point is in the circle; otherwise false
+     *
+     * if the distance from the point to the center of the circle is less than the radius,
+     * then the point is in the circle
      */
+
     public boolean contains(Point p)
     {
-
+        return location.distanceTo(p) <= radius;
     }
 
     /**
@@ -102,10 +109,13 @@ public class Circle
      * otherwise false.
      *
      * @param aCircle a circle.
+     *
+     * if the distance from Circle to aCircle is less than radius,
+     * then the circles intersect
      */
     public boolean intersects(Circle aCircle)
     {
-
+        return location.distanceTo(aCircle.location) <= radius + aCircle.getRadius();
     }
 
     /**
--- src/main/java/Recitation3.java
+++ src/main/java/Recitation3.java
@@ -5,6 +5,31 @@ public class Recitation3 {
     {
         Scanner scan = new Scanner(System.in);
 
-        // add your code here
+        // Get x coordinate
+        System.out.print("Enter x:");
+        int newX = scan.nextInt();
+
+        //Get y coordinate
+        System.out.print("Enter y:");
+        int newY = scan.nextInt();
+
+        /**
+         * Create a circle with those x and y coordinates Create an instance
+         * of a Circle at (x,y) with radius=16.0 called circle16;
+         * Create another instance of a Circle at (14,12) with radius=40.0 called circle40;
+         */
+
+        Circle circle40 = new Circle(new Point(14, 12), 40);
+        Circle circle16 = new Circle(new Point(newX,newY), 16);
+
+        //See if they intersect
+        if (circle40.intersects(circle16))
+        {
+            System.out.println("Circles intersect!");
+        }
+        else {
+            System.out.println("Circles do not intersect!");
+        }
+
     }
 }
